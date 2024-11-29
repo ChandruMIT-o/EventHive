a728d54ef3e3fa65385b65a1b3be436f90597910
Chandru J <chandrukavin0503@gmail.com>
2024-11-29 19:43:31 +0530

    HomePage HTML done.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('EventPage.html')
+    return render_template('HomePage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
