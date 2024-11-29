d562be826e00c1e5eab13b231a169fb8a9725d76
Chandru J <chandrukavin0503@gmail.com>
2024-11-29 21:12:03 +0530

    Create Events Page Done.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('HomePage.html')
+    return render_template('CreateEventPage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
