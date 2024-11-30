cdaa85d65d2912dcc42c8ef0792c3448f923e698
Chandru J <chandrukavin0503@gmail.com>
2024-11-30 12:19:54 +0530

    Admin Dash Board Done.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('RegistrationPage.html')
+    return render_template('EventPage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
